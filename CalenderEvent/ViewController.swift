d6bc9b2245d214a9258bc6f08e5d2febb1ccb692
Naman Sharma <namansharma@Namans-Mac.local>
2017-10-05 11:06:52 -0700

    Initial Commit

---
+++ CalenderEvent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalenderEvent
+//
+//  Created by Naman Sharma on 2017-10-05.
+//  Copyright © 2017 Naman Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
